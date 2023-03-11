18df9aa3a444ebc00dc1303b3a4345570b9810c9
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-03-11 06:36:22 +0100

    feat: first page done

--- openvoices/src/pages/Board/Board.jsx
+++ openvoices/src/pages/Board/Board.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Sidebar, ChannelBar, Chat } from '../../components'
+import styled from 'styled-components'
 
 const Board = () => {
   return (
-    <>
+    <Container>
       <Sidebar />
       <ChannelBar />
       <Chat />
-    </>
+    </Container>
   )
 }
 
+const Container = styled.main`
+  max-width: 1440px;
+  margin: 0 auto;
+`
 export default Board
