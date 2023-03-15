5750c2a730078c6b077251e60ada4164e39163b2
Rasheed Adekunle <102271199+Demandtech@users.noreply.github.com>
2023-03-15 19:13:00 +0100

    Delete frontend directory

--- frontend/README.md
+++
@@ -1,4 +0,0 @@
-## Development
-  1. `cd frontend`
-  2. `npm install`: install dependencies
-  3. `npm start`: start dev server
--- frontend/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import ChannelBar from './Main/ChannelBar';
-import Chat from './Main/Chat';
-import Sidebar from './Main/Sidebar';
-
-export { ChannelBar, Chat, Sidebar };
